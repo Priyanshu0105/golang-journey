ed996f215d4750867606c49fb794b1628a853a31
Priyanshu <168187726+Priyanshu0105@users.noreply.github.com>
2026-03-27 19:53:04 +0530

    add go.mod

---
+++ 03userinput/main.go
@@ -0,0 +1,9 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Priyanshu"
+	age := 21
+	fmt.Println("Hello my name is", name, "and my age is", age)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Priyanshu0105/learn-go
+
+go 1.26.1

f6dfb02a4db15aa2ec7014a3e08e5239ed1a8f98
Priyanshu <168187726+Priyanshu0105@users.noreply.github.com>
2026-03-27 20:55:50 +0530

    user input handling and formatting

--- 03userinput/main.go
+++ 03userinput/main.go
@@ -1,9 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
 
 func main() {
 	name := "Priyanshu"
 	age := 21
 	fmt.Println("Hello my name is", name, "and my age is", age)
+
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Enter your age :")
+	// comma ok || err syntax
+	input, _ := reader.ReadString('\n')
+	fmt.Println("karma is a bitch", input)
+	fmt.Printf("you entered: %T", input)
 }

5ed5e45e989a0b3c5a2df85b04f15ba3050af742
Priyanshu <168187726+Priyanshu0105@users.noreply.github.com>
2026-03-30 22:22:54 +0530

    mathematical calculations and random number generation

---
+++ 05maths/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"math/big"
+
+	//"math/rand/v2"
+	"crypto/rand"
+)
+
+func main() {
+	fmt.Println("The square root of 16 is", math.Sqrt(16))
+	fmt.Println("The value of Pi is", math.Pi)
+	fmt.Println("The value of E is", math.E)
+	fmt.Println("The value of the golden ratio is", math.Phi)
+	// random number
+
+	//fmt.Println(rand.IntN(5) + 1) // generates a random number between 0 and 4, so we add 1 to get a number between 1 and 5
+	//random from crypto
+
+	myRandomNumber, _ := rand.Int(rand.Reader, big.NewInt(6)) //generates a random number between 0 and 5
+	fmt.Println(myRandomNumber)
+}

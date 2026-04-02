030bbebd7b594ad93b692e653aa310374cb1ce73
Priyanshu <168187726+Priyanshu0105@users.noreply.github.com>
2026-04-02 20:12:46 +0530

    add main.go to demonstrate slice and sorting operations in Go

---
+++ 09slices/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+	var fruit = []string{"Apple", "Banana", "Cherry", "Date"}
+	fmt.Printf("type of fruitlist: %T\n", fruit)
+	fmt.Println("The value of fruit is", fruit)
+
+	fruit = append(fruit, "Elderberry", "Fig", "Grape")
+	fmt.Println("The new value of fruit is", fruit)
+
+	fruit = append(fruit[:3])
+	fmt.Println(fruit)
+
+	highScores := make([]int, 4)
+	highScores[0] = 100
+	highScores[1] = 50
+	highScores[2] = 25
+	highScores[3] = 75
+	fmt.Println("High Scores:", highScores)
+
+	highScores = append(highScores, 99, 88, 77)
+	fmt.Println("Updated High Scores:", highScores)
+
+	sort.Ints(highScores)
+	fmt.Println("Sorted High Scores:", highScores)
+	fmt.Println(sort.IntsAreSorted(highScores))
+}
